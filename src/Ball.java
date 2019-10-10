29dd9c64f016f0816e26eaf5982b87be937240e6
Enda Kilgarriff <enda.kilgarriff@gmail.com>
2019-10-10 17:00:43 +0100

    Task 2 complete

--- src/Ball.java
+++ src/Ball.java
@@ -1,35 +1,36 @@
-public enum Ball {
-	BASEBALL(149, 73), 
-	BASKETBALL(624, 239), 
-	FOOTBALL(450, 216),
-	GOLF(46, 43), 
-	POOL(156, 60),
-	SOFTBALL(180, 97),
-	TENNIS(59, 64),
-	VOLLEYBALL(260, 218);
-
-	private final double mass, diameter; // In kilograms
-	private double radius; 
-	
-	Ball(double mass, double diameter) {
-		this.mass = mass;
-		this.diameter = diameter;
-		radius = diameter/2;
+	public enum Ball {
+		BASEBALL(149, 73), 
+		BASKETBALL(624, 239), 
+		FOOTBALL(450, 216),
+		GOLFBALL(46, 43), 
+		POOLBALL(156, 60),
+		SOFTBALL(180, 97),
+		TENNISBALL(59, 64),
+		VOLLEYBALL(260, 218);
 		
+		private final double mass, diameter; // In grams
+		private double radius; 
+		
+		Ball(double mass, double diameter) {
+			this.mass = mass;
+			this.diameter = diameter;
+			radius = diameter/2;
+			
+		}
+		
+		public double mass() { return mass; }
+		public double diameter() { return diameter; }
+		
+		public double getCircumference() {
+			return 2.0*Math.PI*radius;
+		}
+		
+		public double getVolume() {
+			return (4.0*Math.PI*radius)/3.0;
+		}
+		
+		@Override 
+		public String toString() {
+			return  this.name() + "\t" + mass + " g " + diameter + " mm"; 
+		}
 	}
-	
-	public double mass() { return mass; }
-	public double diameter() { return diameter; }
-	
-	public double getCircumference() {
-		return 2.0*Math.PI*radius;
-	}
-	
-	public double getVolume() {
-		return (4.0*Math.PI*radius)/3.0;
-	}
-	
-	
-	
-}
-
---
+++ src/EnumTest.java
@@ -0,0 +1,23 @@
+import java.text.DecimalFormat;
+
+public class EnumTest {
+
+	static DecimalFormat precision = new DecimalFormat("0.00");
+
+	public static void main(String args[]) {
+
+		System.out.println("Task 1");
+		Ball arr[] = Ball.values();
+
+		for (Ball ball : arr) {
+			System.out.println(ball.toString());
+		}
+
+		System.out.println("\nTask 2");
+		System.out.println(Ball.GOLFBALL.name() + " circumference \t"
+				+ precision.format(Ball.GOLFBALL.getCircumference()) + " mm");
+		System.out
+				.println(Ball.BASEBALL.name() + " diameter \t" + precision.format(Ball.BASEBALL.getVolume()) + " m^3");
+
+	}
+}
